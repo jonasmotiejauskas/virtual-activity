fa81a699fa63050542d434fd54581cebdc1d3a81
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-08-07 22:39:56 +0300

    Added a few domain classes for starters

---
+++ AttendencyTracker/Domain/Attendee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AttendencyTracker.Domain
+{
+    public class Attendee
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+
+        public Attendee(string id, string name, string lastName)
+        {
+            var isParsed = Guid.TryParse(id, out var parsedGuid);
+            Id = !isParsed ? throw new ArgumentException("Failed to parse Guid", nameof(id)) : parsedGuid;
+            Name = string.IsNullOrWhiteSpace(name) ? throw new ArgumentNullException(nameof(name)) : name;
+            LastName = string.IsNullOrWhiteSpace(lastName) ? throw new ArgumentNullException(nameof(name)) : lastName;
+        }
+
+        public Attendee(string name, string lastName)
+        {
+            Name = string.IsNullOrWhiteSpace(name) ? throw new ArgumentNullException(nameof(name)) : name;
+            LastName = string.IsNullOrWhiteSpace(lastName) ? throw new ArgumentNullException(nameof(name)) : lastName;
+            Id = Guid.NewGuid();
+        }
+    }
+}
--- AttendencyTracker/Program.cs
+++ AttendencyTracker/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AttendencyTracker.Domain;
+using System;
 
 namespace AttendencyTracker
 {
@@ -6,7 +7,8 @@ namespace AttendencyTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var someAttendee = new Attendee("Jonas", "Motiejauskas");
+            Console.WriteLine($"{someAttendee.Id.ToString()}");
         }
     }
 }
