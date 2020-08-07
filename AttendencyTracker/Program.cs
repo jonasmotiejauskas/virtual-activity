de0cbd1ba7238d90ae2892e77b22d819981119bc
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-08-07 22:17:14 +0300

    Added a new .netcore backend service for tracking attendency

---
+++ AttendencyTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AttendencyTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
