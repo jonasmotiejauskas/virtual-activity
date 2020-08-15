b3425fa14bca0f829db75e8e15689ec885c265d4
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-08-16 00:05:18 +0300

    Exposed app through standard port. Maps to same port everytime. Dockerignore default val (#2)

--- AttendanceTracker/TestClient/Program.cs
+++ AttendanceTracker/TestClient/Program.cs
@@ -9,13 +9,11 @@ namespace TestClient
         static void Main()
         {
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            using var channel = GrpcChannel.ForAddress("http://localhost:32784");
+            using var channel = GrpcChannel.ForAddress("http://localhost:51333");
 
             var client = new AttendeeService.AttendeeServiceClient(channel);
             var reply = client.RegisterAttendee(new RegisterAttendeeRequest { Name = "Charlie", LastName = "Chaplin" });
             Console.WriteLine("Created new attendee: {Name: " + reply.Name + ", LastName: " + reply.LastName + ", Id: " + reply.Id + "}");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
     }
 }
