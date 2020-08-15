e99a5ee039ae4eb629154e90f098df1d84e70437
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-08-15 14:52:50 +0300

    Added a test client to test webservice connectivity

---
+++ AttendanceTracker/TestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using AttendaceTracker.WebService.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Net.Http;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:32782");
+            var client = new AttendeeService.AttendeeServiceClient(channel);
+            var reply = client.RegisterAttendee(new RegisterAttendeeRequest { Name = "Charlie", LastName = "Chaplin" });
+            Console.WriteLine("Created new attendee: {Name: " + reply.Name + ", LastName: " + reply.LastName + ", Id: " + reply.Id + "}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
